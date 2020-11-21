55f50f3db9c9af7725fe4d1cb86bd4440db07049
Pedro Capriles <zearkiatos@Pedros-MacBook-Air.local>
2020-11-21 17:33:20 -0300

    Add models and general clase

---
+++ src/models/Rectangle.cs
@@ -0,0 +1,31 @@
+namespace lsp_labs_with_csharp.src.models
+{
+    public class Rectangle
+    {
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        private int length;
+        public virtual int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        private int width;
+        public virtual int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+
+
+        public int GetArea()
+        {
+            return this.length * this.width;
+        }
+    }
+}
---
+++ src/models/Square.cs
@@ -0,0 +1,28 @@
+using lsp_labs_with_csharp.src.models;
+namespace lsp_labs_with_csharp.src.models
+{
+    public class Square : Rectangle
+    {
+        public Square(int lengthAndWidth) : base(lengthAndWidth, lengthAndWidth)
+        {
+            this.length = lengthAndWidth;
+            this.width = lengthAndWidth;
+        }
+
+        private int width;
+        public override int Width
+        {
+            get { return width; }
+            set { width = value; length = value; }
+        }
+        private int length;
+        public override int Length
+        {
+            get { return length; }
+            set { length = value; width = value;  }
+        }
+        
+
+
+    }
+}
